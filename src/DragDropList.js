f10df7386c297bf9599e5e8b1537c0b175db6f68
Vivek Eswaran <vivekeswaran@Viveks-MacBook-Pro-2.local>
2018-10-26 00:03:54 -0700

    added prop-types package

--- src/DragDropList.js
+++ src/DragDropList.js
@@ -2,109 +2,101 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class DragDropList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...props };
+    constructor(props) {
+        super(props);
+        this.state = {...props};
 
-    this.rowElement = null;
-    this.setrowElement = element => {
-      this.rowElement = element;
-    };
-  }
+        this.rowElement = null;
+        this.setRowElement = element => {
+            this.rowElement = element;
+        };
+    }
 
   getPlaceHolderElement = (item, i) => {
-    var placeholder = document.createElement("li");
+    let placeholder = document.createElement("li");
     placeholder.className = "placeholder";
     placeholder.innerHTML = item;
     placeholder.dataset.id = i;
     return placeholder;
   };
 
-  updateState = () => {
-    var data = this.state.items;
-    var from = Number(this.dragged.dataset.id);
-    var to = Number(this.over.dataset.id);
-    if (from < to) to--;
-    if (this.nodePlacement == "after") to++;
-    data.splice(to, 0, data.splice(from, 1)[0]);
-    this.setState({ items: data });
-  };
+    updateState = () => {
+        let data = this.state.items,
+            from = Number(this.dragged.dataset.id),
+            to = Number(this.over.dataset.id);
+        if (from < to) to--;
+        if (this.nodePlacement == "after") to++;
 
-  onMouseDown = e => {
-    e.currentTarget.parentNode.draggable = true;
-  };
+        data.splice(to, 0, data.splice(from, 1)[0]);
+        this.setState({items: data});
+    };
 
-  dragStart = e => {
-    this.dragged = e.currentTarget;
-    e.dataTransfer.setData("text/html", this.dragged);
-    this.placeholder = this.getPlaceHolderElement(
-      this.dragged.innerHTML,
-      this.dragged.dataset.id
-    );
-  };
+    onMouseDown = e => {
+        e.currentTarget.parentNode.draggable = true;
+    };
 
-  dragEnd = e => {
-    this.dragged.style.display = "block";
-    this.dragged.parentNode.removeChild(this.placeholder);
+    dragStart = e => {
+        this.dragged = e.currentTarget;
+        e.dataTransfer.setData("text/html", this.dragged);
 
-    // update state
-    this.updateState();
-    this.dragged.removeAttribute("draggable");
-  };
+        this.placeholder = this.getPlaceHolderElement(
+            this.dragged.innerHTML,
+            this.dragged.dataset.id
+        );
+    };
 
-  dragOver = e => {
-    e.preventDefault();
-    this.dragged.style.display = "none";
-    if (e.target.className !== "row") return;
-    this.over = e.target;
+    dragEnd = e => {
+        this.dragged.style.display = "block";
+        this.dragged.parentNode.removeChild(this.placeholder);
 
-    var relY = e.clientY - this.over.offsetTop;
-    var height = this.over.offsetHeight / 2;
-    var parent = e.target.parentNode;
+        // update state
+        this.updateState();
+        this.dragged.removeAttribute("draggable");
+    };
 
-    if (relY > height) {
-      this.nodePlacement = "after";
-      parent.insertBefore(this.placeholder, e.target.nextElementSibling);
-    } else if (relY < height) {
-      this.nodePlacement = "before";
-      parent.insertBefore(this.placeholder, e.target);
+    dragOver = e => {
+        e.preventDefault();
+        this.dragged.style.display = "none";
+        if (e.target.className !== "row") return;
+        this.over = e.target;
+
+        let relY = e.clientY - this.over.offsetTop,
+            height = this.over.offsetHeight / 2,
+            parent = e.target.parentNode;
+
+        if (relY > height) {
+            this.nodePlacement = "after";
+            parent.insertBefore(this.placeholder, e.target.nextElementSibling);
+        } else if (relY < height) {
+            this.nodePlacement = "before";
+            parent.insertBefore(this.placeholder, e.target);
+        }
+    };
+
+    render() {
+        const listItems = this.state.items.map((item, i) => {
+            return (
+                <li className="row" data-id={i} key={i} onDragEnd={this.dragEnd} onDragStart={this.dragStart}>
+                    {item}
+                    <button className="icon" onMouseDown={this.onMouseDown} aria-label="Reorder"/>
+                </li>
+            );
+        });
+
+        return (
+            <ul ref={this.setRowElement} onDragOver={this.dragOver}>
+                {listItems}
+            </ul>
+        );
     }
-  };
-
-  render() {
-    var listItems = this.state.items.map((item, i) => {
-      return (
-        <li
-          className="row"
-          data-id={i}
-          key={i}
-          onDragEnd={this.dragEnd}
-          onDragStart={this.dragStart}
-        >
-          {item}
-          <button
-            className="icon"
-            onMouseDown={this.onMouseDown}
-            aria-label="Reorder"
-          />
-        </li>
-      );
-    });
-
-    return (
-      <ul ref={this.setrowElement} onDragOver={this.dragOver}>
-        {listItems}
-      </ul>
-    );
-  }
 }
 
 DragDropList.propTypes = {
-  content: PropTypes.array
+    content: PropTypes.array
 };
 
 DragDropList.defaultProps = {
-  content: []
+    content: []
 };
 
 export default DragDropList;
